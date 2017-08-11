c843fdba6ad78bd903ba4d464172030cbdf01c98
Sergii.Garanenko <garanenkosergii@gmail.com>
2017-08-11 16:25:16 +0300

    CommandService refactored using open/closed principle

--- task_1/sergii.garanenko/src/main/java/Config.java
+++ task_1/sergii.garanenko/src/main/java/Config.java
@@ -1,8 +1,14 @@
+
+import commands.CommitCommand;
+import commands.InitCommand;
+import commands.LogCommand;
+import commands.RevertCommand;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.io.ClassPathResource;
+import services.CommandService;
 
 @SpringBootConfiguration
 @ComponentScan({"services", "model", "utils"})
@@ -14,5 +20,15 @@ public class Config {
     propertyPlaceholderConfigurer
         .setLocation(new ClassPathResource("VCS.properties"));
     return propertyPlaceholderConfigurer;
+  }
+
+  @Bean
+  public CommandService getCommandService() {
+    CommandService commandService = new CommandService();
+    commandService.addCommand(new InitCommand());
+    commandService.addCommand(new RevertCommand());
+    commandService.addCommand(new LogCommand());
+    commandService.addCommand(new CommitCommand());
+    return commandService;
   }
 }
--- task_1/sergii.garanenko/src/main/java/services/CommandService.java
+++ task_1/sergii.garanenko/src/main/java/services/CommandService.java
@@ -1,7 +1,8 @@
 package services;
 
+import commands.Command;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,14 +12,21 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Service
 public class CommandService {
 
   @Autowired
   private VersionControlService controlService;
+  private Command commandChain;
+
+  public void addCommand(Command command) {
+    commandChain = commandChain == null
+        ? command
+        : commandChain.or(command);
+  }
 
   public void listen() {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+    BufferedReader reader = new BufferedReader(
+        new InputStreamReader(System.in, StandardCharsets.UTF_8));
 
     while (true) {
       try {
@@ -31,33 +39,14 @@ public class CommandService {
               .filter(item -> item.length() != 0)
               .collect(Collectors.toList());
           if (commands.size() > 0) {
-            switch (commands.get(0)) {
-              case "init":
-                if (commands.size() > 1) {
-                  controlService.init(commands.get(1));
-                }
-                break;
-              case "commit":
-                if (commands.size() > 1) {
-                  commands.remove(0);
-                  controlService.commit(commands.stream().collect(Collectors.joining(" ")));
-                }
-                break;
-              case "log":
-                System.out.println(controlService.log());
-                break;
-              case "revert":
-                if (commands.size() > 1) {
-                  controlService.revert(commands.get(1));
-                }
-                break;
-              default:
-                break;
+            if (!commandChain.handle(controlService, commands)) {
+              System.out.println("Unknown command");
             }
           }
         }
       } catch (IOException ex) {
         ex.printStackTrace();
+        System.out.println("Some IO error happend. Try again. Detailes shown above");
       }
     }
   }
