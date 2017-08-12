75d5c6eadffb9e47fedd9fc97e26c9c201ab171f
Sergii.Garanenko <garanenkosergii@gmail.com>
2017-08-12 12:43:40 +0300

    Added logging

--- task_1/sergii.garanenko/src/main/java/services/VersionControlService.java
+++ task_1/sergii.garanenko/src/main/java/services/VersionControlService.java
@@ -1,6 +1,8 @@
 package services;
 
 import model.FileData;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import utils.ObjectsRepository;
@@ -13,6 +15,7 @@ import java.util.Set;
 @Service
 public class VersionControlService {
 
+  private Logger logger = LogManager.getLogger();
   private File watchedFolder;
   @Autowired
   private StructureService structureService;
@@ -21,22 +24,31 @@ public class VersionControlService {
 
 
   public void commit(String commitName) throws IOException {
+    logger.traceEntry("Try to commit: " + commitName);
     Set<FileData> fileDataSet = structureService.getProjectStructure();
     objectsRepository.saveSnapshot(fileDataSet, commitName);
+    logger.traceExit("Commited succesfully");
   }
 
   public String log() {
-    return objectsRepository.getLog();
+    logger.traceEntry("Getting log out");
+    String log = objectsRepository.getLog();
+    logger.traceExit("Log obtained successfully");
+    return log;
   }
 
   public void revert(String commitName) throws IOException {
+    logger.traceEntry("Start to revert");
     objectsRepository.revert(commitName);
+    logger.traceExit("Reverted successfully");
   }
 
   public void init(String pathToWatch) throws IOException {
+    logger.traceEntry("Initializing...");
     this.watchedFolder = new File(pathToWatch);
 
     structureService.setWatchedFolder(watchedFolder);
     objectsRepository.createStorage(watchedFolder);
+    logger.traceExit(pathToWatch + " initialized");
   }
 }
