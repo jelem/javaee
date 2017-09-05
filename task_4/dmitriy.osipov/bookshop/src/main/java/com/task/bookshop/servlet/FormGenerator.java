5814471dff810bbc4bd768c2e2158aa31b685907
Dima Osipov <osipov.dmal@gmail.com>
2017-09-05 11:03:23 +0300

    Task 4: Tests (#19)
    * Created models for git imitation
    
    * Updating structure, editing walktrought
    
    * Changed walktrought algorythm
    
    * Possibility for file saving by commits serialization was added
    
    * Fixed checkstyle and findbugs remarks
    
    * Added methods for encrypting and saving all elements to different files
    
    * readme.md was added
    
    * Update readme.md and build.gradle
    
    * Task_1: made required changes
    
    * Task_2: servlets for bookshop
    
    * Readme.md was added
    
    * task_3: bookshop with styles and different jsp
    
    * Init commit & some tests added
    
    * Test for repository file save and load
    
    * servlets refactored for remove HtmlPagePreparer
    
    * HtmlPagePreparer removed, test for LinkGenerator added
    
    * Task 4: Testing

---
+++ task_4/dmitriy.osipov/bookshop/src/main/java/com/task/bookshop/servlet/FormGenerator.java
@@ -0,0 +1,60 @@
+package com.task.bookshop.servlet;
+
+import com.task.bookshop.model.Book;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.nio.charset.Charset;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet/formgen")
+public class FormGenerator extends HttpServlet {
+
+  public FormGenerator() {
+    super();
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    String condition = req.getParameter("condition");
+    if (condition == null) {
+      condition = "title";
+    }
+
+    String title = req.getParameter("title");
+    if (title == null) {
+      title = "названию";
+    }
+
+    req.setAttribute("requestForm",
+        new String(generateFindForm(condition, "get").getBytes(Charset.defaultCharset()),
+            Charset.forName("UTF-8")));
+    RequestDispatcher dispatcher = req.getRequestDispatcher("/jsp/requestform.jsp");
+    dispatcher.forward(req, resp);
+  }
+
+  private String generateFindForm(String condition, String method) {
+    String inputType = "text";
+    try {
+      Field field = Book.class.getDeclaredField(condition);
+      if (field.getType().isAssignableFrom(Number.class)
+          || field.getType().getSimpleName().equals("int")
+          || field.getType().getSimpleName().equals("long")) {
+        inputType = "number";
+      }
+    } catch (NoSuchFieldException exc) {
+      exc.printStackTrace();
+    }
+    return "<form action=\"/bookshop/servlet/find/" + condition + "\" method=\"" + method
+        + "\" class=\"requestForm\">\n\t<input type=\"" + inputType + "\" name=\"" + condition
+        + "\" class=\"formEntry\">"
+        + "\n\t<input type=\"submit\" value=\"Поиск\" class=\"formEntry\">\n</form>";
+  }
+}
