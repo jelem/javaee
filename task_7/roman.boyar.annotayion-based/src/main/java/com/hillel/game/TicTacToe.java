fa518e93d4ba7ba64c3a0a778099417fcc428cb2
rboyar <rboyar@slice.com>
2017-09-20 16:52:15 +0300

    TicTacToe spring refactoring - added annotions-xml-based and annotations-java-based versions

---
+++ task_7/roman.boyar.annotayion-based/src/main/java/com/hillel/game/TicTacToe.java
@@ -0,0 +1,71 @@
+package com.hillel.game;
+
+import com.hillel.game.model.board.TicTacToeBoard;
+import com.hillel.game.model.player.Player;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component("tictactoe")
+public class TicTacToe {
+
+  private TicTacToeBoard board;
+  private Player player1;
+  private Player player2;
+  private Player currentPlayer;
+
+  @Autowired
+  public TicTacToe(@Qualifier("human") Player player1, @Qualifier("robot") Player player2,
+      TicTacToeBoard board) {
+    this.player1 = player1;
+    this.player2 = player2;
+    this.board = board;
+  }
+
+  @Autowired
+   public void setCurrentPlayer(Player currentPlayer) {
+    this.currentPlayer = currentPlayer;
+  }
+
+
+  public void startGame() {
+    while (!gameFinished()) {
+      makeMove();
+      printBoard();
+      changeCurrentPlayer();
+    }
+
+    calculateWinner();
+  }
+
+  private void calculateWinner() {
+    String winner = board.getWinner();
+    System.out.println(winner);
+  }
+
+  private void changeCurrentPlayer() {
+    if (currentPlayer == player1) {
+      currentPlayer = player2;
+    } else {
+      currentPlayer = player1;
+    }
+  }
+
+  private void printBoard() {
+    board.print();
+  }
+
+  // В этом методе данные вводятся с клавиатуры,
+  // такой подход нужен для человека, но не для компьютера.
+  // Перенесите эту функциональность в соответсвующую реализацию
+  // игрока, а в этом методе вызывайте общий метод интерфейса Player
+  private void makeMove() {
+    int[] ints = currentPlayer.makeMove();
+    board.fillCell(ints[0], ints[1], currentPlayer.getSymbol());
+  }
+
+  private boolean gameFinished() {
+    return board.isGameFinished();
+  }
+}
