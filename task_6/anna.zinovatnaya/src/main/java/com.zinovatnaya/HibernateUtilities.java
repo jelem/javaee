8c9bef7860fe67c88f9c8f723a7248bf90e1160b
AnnaZinovatnaya <zinovatnaya@gmail.com>
2017-09-21 19:54:42 +0300

    task 6 hibernate

---
+++ task_6/anna.zinovatnaya/src/main/java/com.zinovatnaya/HibernateUtilities.java
@@ -0,0 +1,22 @@
+package com.zinovatnaya;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtilities {
+
+  private static SessionFactory sessionFactory = null;
+
+  private HibernateUtilities() {
+  }
+
+  public static SessionFactory getSessionFactory() {
+    if (sessionFactory == null) {
+      sessionFactory = new Configuration()
+      .configure()
+      .buildSessionFactory();
+    }
+
+    return sessionFactory;
+  }
+}
