c58f2cb6678d26d58ec843dcd02a21e55d477ab4
DmitriyOsipov <osipov.dmal@gmail.com>
2017-09-24 11:22:52 +0300

    DI without Spring

--- task_7/dmitriy.osipov/src/main/java/com/hillel/game/Main.java
+++ task_7/dmitriy.osipov/src/main/java/com/hillel/game/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
   public static void main(String[] args) {
 
-    TicTacToe ticTacToe = new TicTacToe();
+    TicTacToe ticTacToe = new TicTacToe(new Board(), new PlayerHuman("John", 'X'),
+        new PlayerPc("T100", 'O'));
     ticTacToe.startGame();
 
   }
